4195c0edd7e2481273a8b2ec4f922405970a376c
Everton Rafael Freitas <evertonregistrosecontratos@gmail.com>
2025-08-29 11:52:16 -0300

    Add files via upload

---
+++ ex059.py
@@ -0,0 +1,36 @@
+#Crie um programa que leia dois valores e mostre um menu como o ao lado da tela:
+#Seu programa deverá realizar a operação solicitada em cada caso.
+from time import sleep
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''[1] somar
+    [2] multiplicar
+    [3] maior
+    [4] novos números
+    [5] sair do programa''')
+    opção = int(input('Qual é a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+    elif opção == 2:
+        produto = n1 * n2
+        print('O resultado de {} X {} é {}'.format(n1, n2, produto))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+            print('Entre {} e {} o maior é {}'.format(n1, n2, produto))
+    elif opção == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opção == 5:
+        print('finalizando...')
+    else:
+        print('Opção invélida. Tente novamente')
+    print('=-=' * 10)
+    sleep(2)
+print('Fim do programa! Volte sempre!')
